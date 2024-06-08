dc01edf5c0ff282ccd47bc5da48456d103c620b5
bujanski <b.alan.janssen@gmail.com>
2024-06-07 22:08:36 -0500

    color palette set

--- app/appContext.js
+++ app/appContext.js
@@ -6,39 +6,27 @@ export const appInitialData = {
     recCategories: {
         books: {
             title: "Books",
-            description:
-                "Explore a wide range of books from different genres and authors.",
-            url: "https://example.com/books",
+            url: "/books",
         },
         movies: {
             title: "Movies",
-            description:
-                "Find recommendations for the latest and greatest movies.",
-            url: "https://example.com/movies",
+            url: "/movies",
         },
         musicians: {
             title: "Musicians",
-            description:
-                "Discover new music and artists across various genres.",
-            url: "https://example.com/musicians",
+            url: "/musicians",
         },
         tabletopGames: {
             title: "Tabletop Games",
-            description:
-                "Dive into the world of board games and tabletop adventures.",
-            url: "https://example.com/tabletop-games",
+            url: "/tabletop-games",
         },
         tvShows: {
             title: "TV Shows",
-            description:
-                "Catch up on the most popular and critically acclaimed TV shows.",
-            url: "https://example.com/tv-shows",
+            url: "/tv-shows",
         },
         videoGames: {
             title: "Video Games",
-            description:
-                "Stay updated with the latest video games and industry news.",
-            url: "https://example.com/video-games",
+            url: "/video-games",
         },
     },
 };
